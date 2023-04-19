0bd9c701026db1cdb4961f0e57d0059b374c2482
truongkhongsai <anhp31746@gmail.com>
2023-04-19 13:11:35 +0700

    fist commit

---
+++ toan.java
@@ -0,0 +1,13 @@
+
+public class toan {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        int c = 10;
+
+        System.out.println("a + b * c   = " + (a + b * c));
+        System.out.println("(a + b) * c = " + ((a + b) * c));
+        System.out.println("a / b - c   = " + (a / b - c));
+        System.out.println("a / (b - c) = " + (a / (b - c)));
+    }
+}
---
+++ toantudk.java
@@ -0,0 +1,10 @@
+
+public class toantudk {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+
+        String s = (a % b == 0) ? "a chia het cho b" : "a khong chia het cho b";
+        System.out.println(s);
+    }
+}
